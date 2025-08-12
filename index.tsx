e56cfa09a0dc78800a4214f3235f02e67cb5ed23
semen.ilin <semen.ilin@ridotto.by>
2025-08-12 13:20:30 +0300

    add sw

--- index.tsx
+++ index.tsx
@@ -1,30 +1,27 @@
-import { ScrollViewStyleReset } from 'expo-router/html';
-import type { PropsWithChildren } from 'react';
 
-// This file is web-only and used to configure the root HTML for every
-// web page during static rendering.
-// The contents of this function only run in Node.js environments and
-// do not have access to the DOM or browser APIs.
-export default function Root({ children }: PropsWithChildren) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+export default function Index() {
 
-        {/* Link the PWA manifest file. */}
-        <link rel="manifest" href="/manifest.json" />
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Windows Phone|Mobile|Tablet/i.test(navigator.userAgent);
 
-        {/*
-          Disable body scrolling on web. This makes ScrollView components work closer to how they do on native.
-          However, body scrolling is often nice to have for mobile web. If you want to enable it, remove this line.
-        */}
-        <ScrollViewStyleReset />
 
-        {/* Add any additional <head> elements that you want globally available on web... */}
-      </head>
-      <body>{children}</body>
-    </html>
-  );
+  return (<html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+
+      <link rel="manifest" href="manifest.json" />
+
+
+    </head>
+    <body>
+      {/* http://w25615f0.beget.tech/ */}
+      <iframe src="https://example.com/" width="100%" height='100%' />
+      <script src="pwa.js" />
+    </body>
+  </html>)
+
+
+
 }
