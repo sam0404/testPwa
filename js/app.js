d20668f7e3c83258ba43ef0f2e99ff5c9fb516c6
semen.ilin <semen.ilin@ridotto.by>
2025-08-13 12:49:40 +0300

    fix

--- js/app.js
+++ js/app.js
@@ -23,29 +23,23 @@ closeButton.addEventListener('click', () => {
 
 
 let installEvent = null;
+window.addEventListener('beforeinstallprompt', function (e) {
 
-let installed = false;
-window.addEventListener('beforeinstallprompt', async function (e) {
-
-    //modal.style.display = "block";
+    modal.style.display = "block";
     // Prevent the mini-infobar from appearing on mobile
     e.preventDefault();
     // Stash the event so it can be triggered later.
-    //installEvent = e;
-    e.prompt();
+    installEvent = e;
+});
+
+let installed = false;
+installButton.addEventListener('click', async function () {
+    installEvent.prompt();
     let result = await that.prompt.userChoice;
     if (result && result.outcome === 'accepted') {
         installed = true;
     }
-});
-
-// installButton.addEventListener('click', async function () {
-//     installEvent.prompt();
-//     let result = await that.prompt.userChoice;
-//     if (result && result.outcome === 'accepted') {
-//         installed = true;
-//     }
-// })
+})
 
 window.addEventListener('appinstalled', async function (e) {
     installButton.style.display = "none";
