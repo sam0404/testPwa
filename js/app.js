914d657db73c272b5d943469d76842d6bfd0de89
semen.ilin <semen.ilin@ridotto.by>
2025-08-13 12:44:57 +0300

    show install on start

--- js/app.js
+++ js/app.js
@@ -23,23 +23,29 @@ closeButton.addEventListener('click', () => {
 
 
 let installEvent = null;
-window.addEventListener('beforeinstallprompt', function (e) {
 
-    modal.style.display = "block";
+let installed = false;
+window.addEventListener('beforeinstallprompt', async function (e) {
+
+    //modal.style.display = "block";
     // Prevent the mini-infobar from appearing on mobile
     e.preventDefault();
     // Stash the event so it can be triggered later.
-    installEvent = e;
-});
-
-let installed = false;
-installButton.addEventListener('click', async function () {
+    //installEvent = e;
     installEvent.prompt();
     let result = await that.prompt.userChoice;
     if (result && result.outcome === 'accepted') {
         installed = true;
     }
-})
+});
+
+// installButton.addEventListener('click', async function () {
+//     installEvent.prompt();
+//     let result = await that.prompt.userChoice;
+//     if (result && result.outcome === 'accepted') {
+//         installed = true;
+//     }
+// })
 
 window.addEventListener('appinstalled', async function (e) {
     installButton.style.display = "none";
