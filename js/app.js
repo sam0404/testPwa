d658aa6c055ffc8df1511c7cc5cf43af9520a185
semen.ilin <semen.ilin@ridotto.by>
2025-08-13 11:23:50 +0300

    fix js

--- js/app.js
+++ js/app.js
@@ -10,3 +10,26 @@ if ('serviceWorker' in navigator) {
             });
     });
 }
+
+let installButton = document.createElement('button');
+
+let prompt;
+window.addEventListener('beforeinstallprompt', function (e) {
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    prompt = e;
+});
+
+let installed = false;
+installButton.addEventListener('click', async function () {
+    prompt.prompt();
+    let result = await that.prompt.userChoice;
+    if (result && result.outcome === 'accepted') {
+        installed = true;
+    }
+})
+
+window.addEventListener('appinstalled', async function (e) {
+    installButton.style.display = "none";
+});
